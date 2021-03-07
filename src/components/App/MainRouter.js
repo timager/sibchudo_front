8ba47e2ca9dd85c2c914aa19edfbd4835e39f5e6
timager <timger98@gmail.com>
2021-03-07 23:22:07 +0300

    merge request fixes

--- src/components/App/MainRouter.js
+++ src/components/App/MainRouter.js
@@ -1,47 +1,46 @@
-import React, {Component} from "react";
+import React, { Component } from 'react'
 
 import {
-    BrowserRouter as Router,
-    Route,
-    Switch
-} from 'react-router-dom';
-import HomePage from "../Pages/HomePage/HomePage";
-import CatPage from "../Pages/CatPage/CatPage";
-import CatsPage from "../Pages/CatsPage/CatsPage";
-import AdminMainPage from "../Pages/Admin/AdminMainPage/AdminMainPage";
-import KittensPage from "../Pages/KittensPage/KittensPage";
-import ContactsPage from "../Pages/ContactsPage/ContactsPage";
-import AdminAuthPage from "../Pages/Admin/AdminAuthPage/AdminAuthPage";
-import AdminCatsPage from "../Pages/Admin/AdminCatsPage/AdminCatsPage";
-import LitterPage from "../Pages/LitterPage/LitterPage";
-import AboutCats from "../Pages/AboutPage/AboutCats";
-import Page404 from "../Pages/404/Page404";
-import Page404Admin from "../Pages/404/Page404Admin";
-import AdminLittersPage from "../Pages/Admin/AdminLittersPage/AdminLittersPage";
-
+  BrowserRouter as Router,
+  Route,
+  Switch,
+} from 'react-router-dom'
+import HomePage from '../Pages/HomePage/HomePage'
+import CatPage from '../Pages/CatPage/CatPage'
+import CatsPage from '../Pages/CatsPage/CatsPage'
+import AdminMainPage from '../Pages/Admin/AdminMainPage/AdminMainPage'
+import KittensPage from '../Pages/KittensPage/KittensPage'
+import ContactsPage from '../Pages/ContactsPage/ContactsPage'
+import AdminAuthPage from '../Pages/Admin/AdminAuthPage/AdminAuthPage'
+import AdminCatsPage from '../Pages/Admin/AdminCatsPage/AdminCatsPage'
+import LitterPage from '../Pages/LitterPage/LitterPage'
+import { AboutCats } from '../Pages/AboutPage/AboutCats'
+import Page404 from '../Pages/404/Page404'
+import Page404Admin from '../Pages/404/Page404Admin'
+import AdminLittersPage from '../Pages/Admin/AdminLittersPage/AdminLittersPage'
 
 class MainRouter extends Component {
-    render() {
-        return (
-            <Router>
-                <Switch>
-                    <Route exact path="/" children={<HomePage/>}/>
-                    <Route exact path="/cats" children={<CatsPage/>}/>
-                    <Route exact path="/kittens" children={<KittensPage/>}/>
-                    <Route exact path="/cat/:id" component={CatPage}/>
-                    <Route exact path="/litter/:id" component={LitterPage}/>
-                    <Route exact path="/contacts" children={<ContactsPage/>}/>
-                    <Route exact path="/admin" children={<AdminAuthPage/>}/>
-                    <Route exact path="/admin/main" children={<AdminMainPage/>}/>
-                    <Route exact path="/admin/cats" children={<AdminCatsPage/>}/>
-                    <Route exact path="/admin/litters" children={<AdminLittersPage/>}/>
-                    <Route exact path='/admin/:all' children={<Page404Admin/>}/>
-                    <Route exact path="/about" component={AboutCats}/>
-                    <Route children={<Page404/>}/>
-                </Switch>
-            </Router>
-        )
-    }
+  render () {
+    return (
+      <Router>
+        <Switch>
+          <Route exact path="/" children={<HomePage/>}/>
+          <Route exact path="/cats" children={<CatsPage/>}/>
+          <Route exact path="/kittens" children={<KittensPage/>}/>
+          <Route exact path="/cat/:id" component={CatPage}/>
+          <Route exact path="/litter/:id" component={LitterPage}/>
+          <Route exact path="/contacts" children={<ContactsPage/>}/>
+          <Route exact path="/admin" children={<AdminAuthPage/>}/>
+          <Route exact path="/admin/main" children={<AdminMainPage/>}/>
+          <Route exact path="/admin/cats" children={<AdminCatsPage/>}/>
+          <Route exact path="/admin/litters" children={<AdminLittersPage/>}/>
+          <Route exact path='/admin/:all' children={<Page404Admin/>}/>
+          <Route exact path="/about" component={AboutCats}/>
+          <Route children={<Page404/>}/>
+        </Switch>
+      </Router>
+    )
+  }
 }
 
-export default MainRouter;
+export default MainRouter
--- src/components/Pages/HomePage/HomePage.js
+++ src/components/Pages/HomePage/HomePage.js
@@ -1,54 +1,59 @@
-import React, {Component} from "react";// import CatSlider from "./CatSlider/CatSlider";
+import React, { Component } from 'react'// import CatSlider from
+                                         // "./CatSlider/CatSlider";
 
-import AbstractPage from "../AbstractPage/AbstractPage";
-import TitleH2 from "../../BaseElements/TitleH2/TitleH2";
-import './HomePage.css';
-import Button from "../../BaseElements/Button/Button";
-// import CatSlider from "./CatSlider/CatSlider";
-
+import AbstractPage from '../AbstractPage/AbstractPage'
+import TitleH2 from '../../BaseElements/TitleH2/TitleH2'
+import './HomePage.css'
+import Button from '../../BaseElements/Button/Button'
 
 class HomePage extends Component {
-    render() {
-        return (
-            <AbstractPage title={'Главная - Питомник «Сибирское Чудо» – Коллективный питомник сибирских кошек'}>
-                <TitleH2 text={"О питомнике"}/>
-                <div className={"block"}>
-                    <p>
-                        «Сибирское Чудо» – Коллективный питомник сибирских кошек
-                        Мы рады приветствовать вас на сайте сибирских кошек колорных и традиционных окрасов.
-                    </p>
-                    <p>
-                        Наш питомник заслуженно является одним из самых крупных коллективных питомников в России. Он
-                        зарегистрирован по двум системам WCF и FIFE.
-                    </p>
-                </div>
+  render () {
+    return (
+      <AbstractPage
+        title={'Главная - Питомник «Сибирское Чудо» – Коллективный питомник сибирских кошек'}>
+        <div className={'flex'}>
+          <TitleH2 text={'О питомнике'}/>
+          <Button color={'green'} href={'/about'}>Подробнее</Button>
+        </div>
+        <div className={'block'}>
+          <p>
+            «Сибирское Чудо» – Коллективный питомник сибирских кошек
+            Мы рады приветствовать вас на сайте сибирских кошек колорных и
+            традиционных окрасов.
+          </p>
+          <p>
+            Наш питомник заслуженно является одним из самых крупных коллективных
+            питомников в России. Он
+            зарегистрирован по двум системам WCF и FIFE.
+          </p>
+        </div>
 
-                <div className={'flex'}>
-                    <TitleH2 text={"О наших кошках"}/>
-                    <Button color={'green'} href={"/cats"}>Все кошки</Button>
-                </div>
-                <div className={"block"}>
-                    <div className={'cat_description'}>
-                        Все наши животные привиты и имеют документы. Коты и кошки живут у своих хозяев. Все наши
-                        животные социализированы и отлично ладят с другими кошками. Наши
-                        кошки снимаются в рекламе и приносят победы на выставках!
-                    </div>
-                    {/*<CatSlider/>*/}
-                </div>
+        <div className={'flex'}>
+          <TitleH2 text={'О наших кошках'}/>
+          <Button color={'green'} href={'/cats'}>Все кошки</Button>
+        </div>
+        <div className={'block'}>
+          <div className={'cat_description'}>
+            Все наши животные привиты и имеют документы. Коты и кошки живут у
+            своих хозяев. Все наши
+            животные социализированы и отлично ладят с другими кошками. Наши
+            кошки снимаются в рекламе и приносят победы на выставках!
+          </div>
+        </div>
 
-                <div className={'flex'}>
-                    <TitleH2 text={"Наши котята"}/>
-                    <Button color={'green'} href={"/kittens"}>Все котята</Button>
-                </div>
-                <div className={"block"}>
-                </div>
+        <div className={'flex'}>
+          <TitleH2 text={'Наши котята'}/>
+          <Button color={'green'} href={'/kittens'}>Все котята</Button>
+        </div>
+        <div className={'block'}>
+        </div>
 
-                <TitleH2 text={"Наши достижения"}/>
-                <div className={"block"}>
-                </div>
-            </AbstractPage>
-        );
-    }
+        <TitleH2 text={'Наши достижения'}/>
+        <div className={'block'}>
+        </div>
+      </AbstractPage>
+    )
+  }
 }
 
-export default HomePage;
+export default HomePage
